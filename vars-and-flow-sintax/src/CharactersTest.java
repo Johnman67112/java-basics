2f7a22f506945dcaa972c3259da1acf140f8dd9f
Johnman67112 <joaovitorcucomo@gmail.com>
2022-03-23 23:15:58 -0300

    Characters

---
+++ vars-and-flow-sintax/src/CharactersTest.java
@@ -0,0 +1,19 @@
+
+public class CharactersTest {
+	public static void main(String[] args) {
+		char letter = 'A';
+		System.out.println(letter);
+		
+		char value = 66;
+		System.out.println(value);
+		
+		value = (char) (value + 1);
+		System.out.println(value);
+		
+		String word = "welcome to the dark side";
+		System.out.println(word);
+		
+		word = word + 2020;
+		System.out.println(word);
+	}
+}
