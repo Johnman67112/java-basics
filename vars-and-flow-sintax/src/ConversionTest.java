26921ee3bbdce0e9a77130334b842233658c7baf
Johnman67112 <joaovitorcucomo@gmail.com>
2022-03-23 22:59:22 -0300

    types and vars

---
+++ vars-and-flow-sintax/src/ConversionTest.java
@@ -0,0 +1,28 @@
+
+public class ConversionTest {
+	public static void main(String[] args) {
+		double salary = 1270.50;
+		System.out.println(salary);
+		
+		float floatPoint = 3.14f;
+		System.out.println(floatPoint);
+		
+		int value = (int) salary;
+		System.out.println(value);
+		
+		long bigNumber = 1238714712845l; //l on the end
+		System.out.println(bigNumber);
+		
+		short littleNumber = 2131;
+		System.out.println(littleNumber);
+		
+		byte b = 127;
+		System.out.println(b);
+		
+		double value1 = 0.2;
+		double value2 = 0.1;
+		double total = value1 + value2;
+		
+		System.out.println(total);
+	}
+}
